aee57bda508c35bd20e1d01a03439f252fdbdf23
Witold Hungendorfer <witekh@me.com>
2023-02-01 13:16:17 +0100

    Add iteration

--- shopping.py
+++ shopping.py
@@ -1,1 +1,4 @@
 shopping_list = {'piekarnia':['chleb', 'bułki', 'pączek'], 'warzywniak':['marchew', 'seler', 'rukola']}
+for i in shopping_list:
+    new_list = [elem.capitalize() for elem in shopping_list.get(i)]
+    print("Idę do", i.capitalize(), ", kupuję tu następujące rzeczy: ", new_list)
