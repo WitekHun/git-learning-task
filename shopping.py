0894236a27e21e97ce527058d0a5cc74db43f391
Witold Hungendorfer <witekh@me.com>
2023-02-01 15:23:33 +0100

    Add products sum

--- shopping.py
+++ shopping.py
@@ -1,4 +1,7 @@
 shopping_list = {'piekarnia':['chleb', 'bułki', 'pączek'], 'warzywniak':['marchew', 'seler', 'rukola']}
+products_sum = 0
 for i in shopping_list:
     new_list = [elem.capitalize() for elem in shopping_list.get(i)]
     print("Idę do", i.capitalize(), ", kupuję tu następujące rzeczy: ", new_list)
+    products_sum += len(shopping_list.get(i))
+print("W sumie kupuję", products_sum, 'produktów')
